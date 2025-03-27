e52d2f7431da49ec62d237d97c9a8cd8913541ea
Igor Hey Matos <iheymatos1@gmail.com>
2025-03-26 21:02:57 -0300

    Configuração do DB

--- WebApi/Controllers/PersonagemController.cs
+++ WebApi/Controllers/PersonagemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
 
 namespace WebApi.Controllers
 {
@@ -10,6 +11,13 @@ namespace WebApi.Controllers
     [Route("api/[controller]")]
     public class PersonagemController : ControllerBase
     {
+        private readonly AppDbContext _appDbContext;
 
+        public PersonagemController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        
     }
 }
